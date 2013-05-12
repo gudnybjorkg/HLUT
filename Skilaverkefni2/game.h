2cb24693d122311b5d05d7daabd91aa9f2fc4802
Guðný Björk <gudnybjorkg@gmail.com>
2013-05-12 18:38:04 +0000

    abstract functions added

--- Skilaverkefni2/game.h
+++ Skilaverkefni2/game.h
@@ -7,8 +7,8 @@ struct Player{
 public:
     Player();
 private:
-    int m_noPlayers;
     int m_score;
+    int noPawns;
 };
 
 struct Board{
@@ -34,7 +34,23 @@ public:
     GamePlay(int gametype);
     virtual ~GamePlay();
     virtual bool legalMove() = 0;
+    //Part 2
+    void listOfGames();
+    void quit();
+
+    virtual void start() = 0;
+    virtual int getNopawns(Player player) = 0;
+    virtual void legal() = 0;
+    virtual void make(int from_col, int from_row, int to_col, int to_row) = 0;
+    virtual void retract() = 0;
+    virtual void display() = 0;
+    virtual void level(int difficulty) = 0;
+    virtual void evaluate(Board board) = 0;
+    virtual void go() = 0;
+    virtual void debug() = 0;
+
 private:
+    int difficulty;
     int m_gametype;
     Board m_board;
     Player p1;
