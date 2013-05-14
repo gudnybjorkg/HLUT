4a50adc335113c4177900a8a9461ec3fdd9991f1
Þorvarður Örn Einarsson <Thorrik58@gmail.com>
2013-05-14 00:00:22 +0000

    ataxx
    breytti einhverju i h skránni þar með getturum og setTile og þarf
    hugsanlega að breyta board í class og eitthvað !

--- Skilaverkefni2/game.cpp
+++ Skilaverkefni2/game.cpp
@@ -24,6 +24,10 @@ Board::Board(int length, int width){
         }
     }
 }
+
+void Board::setTile(int col, int row, char letter){
+    tiles[col][row] = *new Piece(letter);
+}
 /*-------------------------------------------------------------
 
 Implementation of the GamePlay class
@@ -33,12 +37,40 @@ GamePlay::GamePlay(){
     m_p1 = Player();
     m_p2 = Player();
     m_turns = 0;
-    if(m_gametype == 0)
+    /*if(m_gametype == 0)    // not needed since we just use the corresponding game when the user picks
         m_board = Board(7,7);
     else if(m_gametype == 1)
         m_board = Board(8,8);
+     */   
 }
 GamePlay::~GamePlay(){
 }
 
+
+
+/*-------------------------------------------------------------
+ 
+ Implementation of the atixx gameplay class
+ 
+ ---------------------------------------------------------------*/
+
+Atixx::Atixx(){
+    Piece p1('P');
+    Piece p2('p');
+    
+    p1.setLocation(7, 0); //player 1 set in left bottom corner 
+    p1.setLocation(0, 7); //player 1 set in right top corner
+    p2.setLocation(0, 0); //player 2 set in right bottom corner
+    p2.setLocation(7, 7); //player 2 set in right bottom corner
+}
+
+bool Atixx::legalMove(int from_col, int from_row, int to_col, int to_row)
+{
+    if ((-2<=(from_col - to_col)<=2) && (-2<=(from_row - to_row)<=2)) //checks if the move is 2 blocks away
+    {
+        return true;
+    }
+    else
+        return false;
+}
 
--- Skilaverkefni2/game.h
+++ Skilaverkefni2/game.h
@@ -4,14 +4,16 @@
 
 enum Game{ Breakthrough = 0, Ataxx = 1};
 
+//missing setting pieces on the board when they are set on location. Get board in order to set tiles?
+
 class Piece{
 public:
     Piece(){m_type = '.'; m_owner = -1;}
-    Piece(char type){m_type = type;}
+    Piece(char type){m_type = type; if (isupper(type)){setOwner(1);} else setOwner(0);} //if capital p1 else p2
     void setOwner(int owner){m_owner = owner;}
-    int getOwner(){ return 1;}
+    int getOwner(){ return m_owner;}
     std::pair<int, int> getLocation(Piece piece){ return piece.m_location; }
-    void setLocation(int x, int y){ m_location = std::make_pair(x,y); }
+    void setLocation(int x, int y){ m_location = std::make_pair(x,y);} //needs to set it on the tiles
     virtual ~Piece(){}
 private:
     char m_type;
@@ -26,9 +28,11 @@ private:
     std::pair<int, int> m_lastLocation;
     int m_score;
     std::vector<Piece> m_pieces;
+    int noOfPieces; //added inn, maybe easier to have this. Everytime we place a piece on the board add
+                    //to the count
 };
 
-struct Board{
+class Board{
 public:
     /*Constructs a square board with a specific size of length and width.
            L  e  n  g  t  h
@@ -40,6 +44,7 @@ public:
     */
     Board(){}
     Board(int length, int width);
+    void setTile(int col, int row, char letter);
 private:
     int m_length;
     int m_width;
@@ -50,7 +55,7 @@ class GamePlay{
 public:
     GamePlay();
     virtual ~GamePlay();
-    virtual bool legalMove() = 0;
+    virtual bool legalMove(int from_col, int from_row, int to_col, int to_row) = 0;
 
     //Part 2
     void listOfGames();
@@ -70,10 +75,19 @@ public:
 private:
     int m_turns;
     int m_difficulty;
-    int m_gametype;
-    Board m_board;
+    //int m_gametype;  //not necessary i think?
+    Board m_board; //think you dont have to initialize the board until in the game you choose to play
     Player m_p1;
     Player m_p2;
+};
+
+class Atixx : public GamePlay{
+public:
+    Atixx();      //set up the board and the game for ataxx
+    virtual bool legalMove(int from_col, int from_row, int to_col, int to_row);
+private:
+    Board m_board;
+    
 };
 
 
